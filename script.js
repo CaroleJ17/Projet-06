3f24e5901de81498266cb8251ea3b03e5b6d08b9
Carole JEGOU <carole.jegou41@sfr.fr>
2023-12-14 12:47:50 +0100

    first

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const urlWorks = "http://localhost:5678/api/works";
 
 fetch(urlWorks)
   .then((response) => response.json())
-  .then((response) => alert(JSON.stringify(response)))
+  .then((data) => alert(JSON.stringify(data)))
   .catch((error) => alert("Erreur : " + error));
