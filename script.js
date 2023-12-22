96c2b080b9bc4c6f56f542fe9356dd056a0d1740
Carole JEGOU <carole.jegou41@sfr.fr>
2023-12-22 11:47:13 +0100

    Etape 1.1

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 const urlWorks = "http://localhost:5678/api/works";
 
 fetch(urlWorks)
-  .then((response) => response.json())
-  .then((data) => alert(JSON.stringify(data)))
-  .catch((error) => alert("Erreur : " + error));
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Erreur");
+    }
+    return response.json();
+  })
+
+  .then((data) => {
+    const gallery = document.querySelector(".gallery");
+    for (const elements of data) {
+      const project = createWork(elements);
+      gallery.appendChild(project);
+    }
+  });
+
+function createWork(elements) {
+  const project = document.createElement("figure");
+  const img = document.createElement("img");
+  const imgTitle = document.createElement("figcaption");
+
+  img.src = elements.imageUrl;
+  imgTitle.innerText = elements.title;
+
+  project.appendChild(img);
+  project.appendChild(imgTitle);
+
+  project.classList.add("project");
+  project.setAttribute("data-category", elements.category.name);
+
+  return project;
+}
+
+// Filtres
+
+fetch("http://localhost:5678/api/categories").then((response) => {
+  if (!response.ok) {
+    throw new Error("Erreur");
+  }
+  return response.json();
+});
